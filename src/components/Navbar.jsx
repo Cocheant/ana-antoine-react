1321ee018f236ea9f292384f1958020a983cbbf8
Cocheant <entonia01@gmail.com>
2024-03-12 14:06:31 +0100

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -48,13 +48,28 @@ const Navbar = () => {
                     : 'opacity-0 transform -translate-y-full'
                 } transition-transform absolute top-0 left-0 w-full h-screen bg-zinc-800/80 flex flex-col justify-center items-center text-2xl`}
                 onClick={() => setNav(false)}
-              >
-                <Link to='/'><li className='hover:text-teal-700'>Home</li></Link>
-                <Link to='/features'><li className='hover:text-teal-700'>Features</li></Link>
-                <Link to='/products'><li className='hover:text-teal-700'>Products</li></Link>
-                <Link to='/about'><li className='hover:text-teal-700'>About</li></Link>
-                <Link to='/locations'><li className='hover:text-teal-700'>Locations</li></Link>
-                <Link to='/contact'><li className='hover:text-teal-700'>Contact Us</li></Link>
+              >    
+                  <NavLink to="/story" className={({ isActive }) => isActive ? 'text-amber-700' : 'text-orange-300'}>
+                    <FormattedMessage
+                        id="navbar.story"
+                        defaultMessage="Our Story"
+                    />
+
+                </NavLink>
+
+                <NavLink to="/travelstay" className={({ isActive }) => isActive ? 'text-amber-700' : 'text-orange-300'}>
+                    <FormattedMessage
+                        id="navbar.travelstay"
+                        defaultMessage="Travel & Stay"
+                    />
+                </NavLink>
+                <NavLink to="/contact" className={({ isActive }) => isActive ? 'text-amber-700' : 'text-orange-300'}>
+                    <FormattedMessage
+                        id="navbar.contact"
+                        defaultMessage="Contact"
+                    />
+                </NavLink>
+
               </ul>
                 */
             </nav>
