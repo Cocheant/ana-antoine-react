45ba43168eeb86c443ff075909a6d61a3c8e6d13
Cocheant <entonia01@gmail.com>
2024-03-12 14:04:41 +0100

    Update Navbar.jsx
    added Hamburger menu commented for lates

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,7 @@
 import { NavLink } from "react-router-dom"
 import { FormattedMessage } from 'react-intl';
+import { RxHamburgerMenu } from "react-icons/rx";
+import { FaTimes } from 'react-icons/fa'
 
 const Navbar = () => {
     return (
@@ -34,6 +36,27 @@ const Navbar = () => {
 
                 </NavLink>
 
+/*
+              <div className=' md:hidden z-10' onClick={handleClick}>
+                {nav ? <FaTimes size={25} color='white' /> : <RxHamburgerMenu size={25}/>}
+              </div>
+              {/* Mobile Menu */}
+              <ul
+                className={`${
+                  nav
+                    ? 'text-white opacity-100 transform translate-x-0'
+                    : 'opacity-0 transform -translate-y-full'
+                } transition-transform absolute top-0 left-0 w-full h-screen bg-zinc-800/80 flex flex-col justify-center items-center text-2xl`}
+                onClick={() => setNav(false)}
+              >
+                <Link to='/'><li className='hover:text-teal-700'>Home</li></Link>
+                <Link to='/features'><li className='hover:text-teal-700'>Features</li></Link>
+                <Link to='/products'><li className='hover:text-teal-700'>Products</li></Link>
+                <Link to='/about'><li className='hover:text-teal-700'>About</li></Link>
+                <Link to='/locations'><li className='hover:text-teal-700'>Locations</li></Link>
+                <Link to='/contact'><li className='hover:text-teal-700'>Contact Us</li></Link>
+              </ul>
+                */
             </nav>
 
         </header>
